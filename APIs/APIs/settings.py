14fd1740bab463bd7ed1d6827395c07d797b0a73
WEAPONCHUA <1063698757@qq.com>
2023-04-01 11:51:59 +0800

    the first commit

--- APIs/APIs/settings.py
+++ APIs/APIs/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f82tn0-ly4o1n7@t36qvw#s2e*twjaf!$c(v1b*m5lgr0ow_+-'
+SECRET_KEY = 'XXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
